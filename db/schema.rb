995f030e0144ef9d01eafe5cca029aca1e86779e
kisimov <skisimov@gmail.com>
2017-02-07 22:53:37 +0300

    add products to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207190740) do
+ActiveRecord::Schema.define(version: 20170207193012) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
