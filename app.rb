3c75251699f08437226da1c42773fdb80daf6733
kisimov <skisimov@gmail.com>
2017-02-20 22:17:40 +0300

    display message if cart is empty

--- app.rb
+++ app.rb
@@ -35,6 +35,10 @@ post '/cart' do
 	@orders_input = params[:orders]
 	@items = parse_orders_input @orders_input
 
+	if @items.length == 0
+		return erb :cart_is_empty
+	end
+
 	@items.each do |item|
 		# id, cnt
 		item[0] = Product.find(item[0])
